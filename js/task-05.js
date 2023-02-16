e74778163224ebe3b66137d2c1796286b89e5a2b
Олена Пестова <nowalena9@gmail.com>
2023-02-17 01:20:28 +0200

    task-05  done

---
+++ js/task-05.js
@@ -0,0 +1,16 @@
+//Напиши скрипт, який під час набору тексту 
+//в інпуті input#name-input (подія input), підставляє
+// його поточне значення в span#name-output. 
+//Якщо інпут порожній, у спані повинен відображатися 
+//рядок "Anonymous".
+
+const inputNode = document.querySelector('#name-input');
+const outputNode= document.querySelector('#name-output');
+//console.log(inputNode);
+console.log(outputNode);
+inputNode.addEventListener('input',(event) =>{
+    console.log(event.currentTarget);
+    outputNode.textContent = event.currentTarget.value;
+    if (event.currentTarget.value ==="")outputNode.textContent = 'Anonymous';
+});
+//value в input це введений контент/ а в події event.currentTarget.value
